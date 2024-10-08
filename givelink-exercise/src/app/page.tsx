82f3bb157698f9c67fa5d8ced815766ed963031a
Konstantinos Kotorenis <kostas123koto@hotmail.gr>
2024-10-08 17:21:18 +0300

    put each product into boxes

--- givelink-exercise/src/app/page.tsx
+++ givelink-exercise/src/app/page.tsx
@@ -30,15 +30,27 @@ export default function Home() {
 
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <p>hello world</p>
+        <h1 className="text-4xl font-bold">Products</h1>
         {data && (
-          <pre>
-            {JSON.stringify(
-              Object.values(data.products).filter((product: any) => product.active),
-              null,
-              2
-            )}
-          </pre>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            {Object.values(data.products)
+              .filter((product: any) => product.active)
+              .map((product: any) => (
+              <div key={product.id} className="card">
+                <Image
+                src={` https://be.givelink.app/images/products/${product.imagePath}`}
+                alt={product.name}
+                width={200}
+                height={200}
+                className="card-image"
+                />
+                <div className="card-content">
+                  <h2 className="card-title">{product.name}</h2>
+                  <p className="card-price">{product.price.toFixed(2)}€</p>
+                </div>
+              </div>
+              ))}
+            </div>
         )}
       </main>
     </div>
